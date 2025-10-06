d391aeb95599c62831119e8fbc0210ed1ea5a693
MD Ansar Ali <67516082+ansarali41@users.noreply.github.com>
2025-10-06 21:31:52 +0600

    Time: 42 ms (39.11%), Space: 70.1 MB (84.99%) - LeetHub

---
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.ts
@@ -0,0 +1,20 @@
+class RecentCounter {
+    private calls: number[];
+    constructor() {
+        this.calls = [];
+    }
+
+    ping(t: number): number {
+        this.calls.push(t);
+        while (this.calls[0] < t - 3000) {
+            this.calls.shift();
+        }
+        return this.calls.length;
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
