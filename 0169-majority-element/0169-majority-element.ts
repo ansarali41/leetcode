c3ac3f971a9361b72f821dd0c514bbbba278419f
MD Ansar Ali <67516082+ansarali41@users.noreply.github.com>
2025-11-03 17:36:29 +0600

    Time: 4 ms (54.63%), Space: 59.5 MB (42.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,11 +1,7 @@
-//[3,2,3]
 function majorityElement(nums: number[]): number {
     const sortedNums: number[] = nums.sort((a, b) => a - b);
-    const mIndex: number = 0;
 
-    if (nums.length === 1) {
-        return nums[0]
-    }
+    if (nums.length === 1) return nums[0]
 
     return nums[Math.floor(nums.length / 2)];
 
